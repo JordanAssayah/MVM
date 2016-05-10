70571d8e750f1571fcf14fb2a76263d636636f94
Jordan Assayah <jordan.assayah@liip.ch>
2016-05-10 10:51:31 +0200

    Add the base of the application

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+var sideMenu = $('.side-menu-mmwf');
+
+function toggleSideMenu(){
+  if(sideMenu.css('margin-left')=='0px'){
+    sideMenu.css('margin-left','-300px');
+    sideMenu.css('transition','margin-left ease-in 0.4s');
+    $('.nav-toggle').removeClass('is-active');
+  }
+  else{
+    sideMenu.css('margin-left','0');
+    sideMenu.css('transition','margin-left ease-out 0.4s');
+    $('.nav-toggle').addClass('is-active');
+  }
+}
+
+function addClassLoading(element){
+  $(element).addClass('is-loading');
+  $(element).addClass('is-disabled');
+}
