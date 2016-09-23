1d5a3393a74cd18f8b2f4c10424e0c191084ebe5
Jordan Assayah <jordan.assayah@apptitude.ch>
2016-09-23 10:21:08 +0200

    MVM: Update vuex indentation

--- app/vuex/actions.js
+++ app/vuex/actions.js
@@ -9,285 +9,284 @@ import Particle from 'particle-api-js'
 
 // MyAccount
 const loginToParticle = ({ dispatch, state }, email, password) => {
-    let particle = new Particle()
-    if (email && password) {
-        particle.login({username: email, password: password}, dispatch('LOADING', true)).then(
-            success => {
-                dispatch('UPDATE_PARTICLE_TOKEN', success.body.access_token)
-                dispatch('LOADING', false)
-                listDevices({ dispatch, state })
-            },
-            error => {
-                dispatch('SET_ERROR', error)
-                dispatch('SHOW_ERROR', true)
-            }
-        )
-    }
+  let particle = new Particle()
+  if (email && password) {
+    particle.login({ username: email, password: password }, dispatch('LOADING', true)).then(
+      success => {
+        dispatch('UPDATE_PARTICLE_TOKEN', success.body.access_token)
+        dispatch('LOADING', false)
+        listDevices({ dispatch, state })
+      },
+      error => {
+        dispatch('SET_ERROR', error)
+        dispatch('SHOW_ERROR', true)
+      }
+    )
+  }
 }
 
 const listDevices = ({ dispatch, state }) => {
-    let particle = new Particle()
-    if (state.particleToken) {
-        particle.listDevices({ auth: state.particleToken }).then(
+  let particle = new Particle()
+  if (state.particleToken) {
+    particle.listDevices({ auth: state.particleToken }).then(
+      success => {
+        let listDevices = success
+        for (let i = 0; i < listDevices.body.length; i++) {
+          particle.getDevice({ deviceId: listDevices.body[i].id, auth: state.particleToken }).then(
             success => {
-                let listDevices = success
-                for (let i = 0; i < listDevices.body.length; i++) {
-                    particle.getDevice({ deviceId: listDevices.body[i].id, auth: state.particleToken }).then(
-                        success => {
-                            let particleDevices = [...state.particleDevices]
-                            if (particleDevices.length < 1) {
-                                success.body.sensors = loadSensorsConfig()
-                                dispatch('PUSH_PARTICLE_DEVICE', success.body)
-                            } else {
-                                for (let j = 0; j < particleDevices.length; j++) {
-                                    if (listDevices.body[i].id === particleDevices[j].id) {
-                                        dispatch('UPDATE_PARTICLE_DEVICE', j, success.body)
-                                    } else if (listDevices.body.length !== particleDevices.length) {
-                                        success.body.sensors = loadSensorsConfig()
-                                        dispatch('PUSH_PARTICLE_DEVICE', success.body)
-                                    }
-                                }
-                            }
-                        },
-                        error => {
-                            dispatch('SET_ERROR', error)
-                            dispatch('SHOW_ERROR', true)
-                        }
-                    )
+              let particleDevices = [...state.particleDevices]
+              if (particleDevices.length < 1) {
+                success.body.sensors = loadSensorsConfig()
+                dispatch('PUSH_PARTICLE_DEVICE', success.body)
+              } else {
+                for (let j = 0; j < particleDevices.length; j++) {
+                  if (listDevices.body[i].id === particleDevices[j].id) {
+                    dispatch('UPDATE_PARTICLE_DEVICE', j, success.body)
+                  } else if (listDevices.body.length !== particleDevices.length) {
+                    success.body.sensors = loadSensorsConfig()
+                    dispatch('PUSH_PARTICLE_DEVICE', success.body)
+                  }
                 }
+              }
             },
             error => {
-                dispatch('SET_ERROR', error)
-                dispatch('SHOW_ERROR', true)
+              dispatch('SET_ERROR', error)
+              dispatch('SHOW_ERROR', true)
             }
-        )
-    }
+          )
+        }
+      },
+      error => {
+        dispatch('SET_ERROR', error)
+        dispatch('SHOW_ERROR', true)
+      }
+    )
+  }
 }
 
 const updateParticleDevices = ({ dispatch, state }) => {
-    if (state.particleToken) {
-        let timerId = setInterval(() => {
-            listDevices({ dispatch, state })
-        }, 7000)
-        dispatch('TIMER_ID_OF_UPDATE_LIST_DEVICES', timerId)
-    }
+  if (state.particleToken) {
+    let timerId = setInterval(() => {
+      listDevices({ dispatch, state })
+    }, 7000)
+    dispatch('TIMER_ID_OF_UPDATE_LIST_DEVICES', timerId)
+  }
 }
 
 const clearIntervalForTimer = ({ dispatch, state }) => {
-    dispatch('CLEAR_INTERVAL', state.updateTimerIdForListDevices)
+  dispatch('CLEAR_INTERVAL', state.updateTimerIdForListDevices)
 }
 
-const saveDeviceConfig = function ({ dispatch, state }, config) {
-    // console.log(config)
+const saveDeviceConfig = function({ dispatch, state }, config) {
+  // console.log(config)
 }
 
 const saveSensorOfDeviceConfig = ({ dispatch, state }, deviceIndex, sensor, type, value) => {
-    dispatch('UPDATE_DEVICE_CONFIG', deviceIndex, sensor, type, value)
+  dispatch('UPDATE_DEVICE_CONFIG', deviceIndex, sensor, type, value)
 }
 
 const analogRead = ({ dispatch, state }, token, deviceId, analogPin) => {
-    let particle = new Particle()
-    if (token) {
-        setInterval(() => {
-            particle.callFunction({ deviceId: deviceId, name: 'analogRead', argument: analogPin, auth: token }).then(
-                success => {
-                    console.log(success)
-                },
-                error => {
-                    dispatch('SET_ERROR', error)
-                    dispatch('SHOW_ERROR', true)
-                }
-            )
-        }, 30)
-    }
+  let particle = new Particle()
+  if (token) {
+    setInterval(() => {
+      particle.callFunction({ deviceId: deviceId, name: 'analogRead', argument: analogPin, auth: token }).then(
+        success => {
+          console.log(success)
+        },
+        error => {
+          dispatch('SET_ERROR', error)
+          dispatch('SHOW_ERROR', true)
+        }
+      )
+    }, 30)
+  }
 }
 
 const digitalRead = ({ dispatch, state }, token, deviceId, digitalPin) => {
-    let particle = new Particle()
-    if (token) {
-        setInterval(() => {
-            particle.callFunction({ deviceId: deviceId, name: 'digitalRead', argument: digitalPin, auth: token }).then(
-                success => {
-                    console.log(success)
-                },
-                error => {
-                    dispatch('SET_ERROR', error)
-                    dispatch('SHOW_ERROR', true)
-                }
-            )
-        }, 30)
-    }
+  let particle = new Particle()
+  if (token) {
+    setInterval(() => {
+      particle.callFunction({ deviceId: deviceId, name: 'digitalRead', argument: digitalPin, auth: token }).then(
+        success => {
+          console.log(success)
+        },
+        error => {
+          dispatch('SET_ERROR', error)
+          dispatch('SHOW_ERROR', true)
+        }
+      )
+    }, 30)
+  }
 }
 
 const sendSignalUp = ({ dispatch, state }, token, deviceId, isConnected) => {
-    let particle = new Particle()
-    if (token && isConnected) {
-        particle.signalDevice({ deviceId: deviceId, signal: true, auth: token }).then(
-            success => {
-                console.log(success)
-            },
-            error => {
-                dispatch('SET_ERROR', error)
-                dispatch('SHOW_ERROR', true)
-            }
-        )
-    }
+  let particle = new Particle()
+  if (token && isConnected) {
+    particle.signalDevice({ deviceId: deviceId, signal: true, auth: token }).then(
+      success => {
+        console.log(success)
+      },
+      error => {
+        dispatch('SET_ERROR', error)
+        dispatch('SHOW_ERROR', true)
+      }
+    )
+  }
 }
 
 const sendSignalDown = ({ dispatch, state }, token, deviceId, isConnected) => {
-    let particle = new Particle()
-    if (token && isConnected) {
-        particle.signalDevice({ deviceId: deviceId, signal: false, auth: token }).then(
-            success => {
-                console.log(success)
-            },
-            error => {
-                dispatch('SET_ERROR', error)
-                dispatch('SHOW_ERROR', true)
-            }
-        )
-    }
+  let particle = new Particle()
+  if (token && isConnected) {
+    particle.signalDevice({ deviceId: deviceId, signal: false, auth: token }).then(
+      success => {
+        console.log(success)
+      },
+      error => {
+        dispatch('SET_ERROR', error)
+        dispatch('SHOW_ERROR', true)
+      }
+    )
+  }
 }
 
 const addLoop = ({ dispatch, state }, loopName, loopDevicesIdToListenTo) => {
-    let loopId = state.numberOfLoops + 1
-    const AudioContext = state.audioContext
-    let loop = setDefaultLoopConfig(AudioContext, loopId, loopName, loopDevicesIdToListenTo)
-    dispatch('PUSH_NEW_LOOP', loop)
+  let loopId = state.numberOfLoops + 1
+  const AudioContext = state.audioContext
+  let loop = setDefaultLoopConfig(AudioContext, loopId, loopName, loopDevicesIdToListenTo)
+  dispatch('PUSH_NEW_LOOP', loop)
 }
 
 const createAndConnectOscillator = (audio) => {
-    let oscillator = audio.createOscillator()
-    oscillator.type = 'square'
-    oscillator.frequency.value = 0
-    oscillator.connect(audio.destination)
+  let oscillator = audio.createOscillator()
+  oscillator.type = 'square'
+  oscillator.frequency.value = 0
+  oscillator.connect(audio.destination)
 
-    return oscillator
+  return oscillator
 }
 
 const createAndConnectSound = (audio, soundBuffer) => {
-    let soundNode = audio.createBufferSource()
-    soundNode.connect(audio.destination)
-    soundNode.buffer = soundBuffer
+  let soundNode = audio.createBufferSource()
+  soundNode.connect(audio.destination)
+  soundNode.buffer = soundBuffer
 
-    return soundNode
+  return soundNode
 }
 
 const recordLoop = (mediaRecorder, oscillator, soundNode, running) => {
-    if (!running) {
-        running = true
-        createAndConnectOscillator()
-        createAndConnectSound()
-        oscillator.start()
-        setTimeout(() => { soundNode.start() }, 2000)
-        mediaRecorder.start()
-    } else {
-        running = false
-        mediaRecorder.stop()
-        oscillator.stop()
-        soundNode.stop()
-    }
+  if (!running) {
+    running = true
+    createAndConnectOscillator()
+    createAndConnectSound()
+    oscillator.start()
+    setTimeout(() => { soundNode.start() }, 2000)
+    mediaRecorder.start()
+  } else {
+    running = false
+    mediaRecorder.stop()
+    oscillator.stop()
+    soundNode.stop()
+  }
 }
 
 // See http://stackoverflow.com/questions/23986953/blob-saved-as-object-object-nodejs
 const blobToBase64 = (blob, cb) => {
-    let reader = new window.FileReader()
-    reader.onload = () => {
-        var dataUrl = reader.result
-        var base64 = dataUrl.split(',')[1]
-        cb(base64)
-    }
-    reader.readAsDataURL(blob)
+  let reader = new window.FileReader()
+  reader.onload = () => {
+    var dataUrl = reader.result
+    var base64 = dataUrl.split(',')[1]
+    cb(base64)
+  }
+  reader.readAsDataURL(blob)
 }
 
 const loadSensorsConfig = () => {
-    return [
-        { IO: 'A0', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
-        { IO: 'A1', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
-        { IO: 'A2', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
-        { IO: 'A3', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
-        { IO: 'A4', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
-        { IO: 'A5', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' }
+  return [
+    { IO: 'A0', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
+    { IO: 'A1', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
+    { IO: 'A2', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
+    { IO: 'A3', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
+    { IO: 'A4', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' },
+    { IO: 'A5', activeFrom: 0, volume: 0, useVelocitySensor: false, toggleSensor: false, soundPath: '' }
     ]
 }
 
-const loadSoundConfig = () => {
-}
+const loadSoundConfig = () => {}
 
 const setDefaultLoopConfig = (AudioContext, loopId, loopName, loopDevicesIdToListenTo) => {
-    let audio = AudioContext
-    let dest = audio.createMediaStreamDestination()
-    let mediaRecorder = new window.MediaRecorder(dest.stream)
-    let chunks = []
-    let running = false
-    let oscillator = null
+  let audio = AudioContext
+  let dest = audio.createMediaStreamDestination()
+  let mediaRecorder = new window.MediaRecorder(dest.stream)
+  let chunks = []
+  let running = false
+  let oscillator = null
 
-    return { id: loopId, loopName, loopDevicesIdToListenTo, audio, chunks, mediaRecorder, running, oscillator }
+  return { id: loopId, loopName, loopDevicesIdToListenTo, audio, chunks, mediaRecorder, running, oscillator }
 }
 
 const setMediaRecorderEventsForLoop = ({ dispatch, state }, loopId) => {
-    let mediaRecorder, chunks
-    const loops = state.loops
-    for (let loop of loops) {
-        if (loop.id === loopId) {
-            mediaRecorder = loop.mediaRecorder
-            chunks = loop.chunks
-        }
+  let mediaRecorder, chunks
+  const loops = state.loops
+  for (let loop of loops) {
+    if (loop.id === loopId) {
+      mediaRecorder = loop.mediaRecorder
+      chunks = loop.chunks
     }
-    mediaRecorder.ondataavailable = (event) => {
-        chunks.push(event.data)
-    }
-    mediaRecorder.onstop = (eve) => {
-        // Make blob out of our blobs, and open it.
-        let blob = new window.Blob(this.chunks, { 'type': 'audio/wav; codecs=1' })
-        blobToBase64(blob, (base64) => { // encod
-            let buf = new window.Buffer(base64, 'base64') // decod
-            console.log(buf)
-            /* fs.writeFile('./static/test.wav', buf, function (error) {
-                if (error) {
-                    console.log(error)
-                } else {
-                    return 'ok'
-                }
-            }) */
-        })
-    }
+  }
+  mediaRecorder.ondataavailable = (event) => {
+    chunks.push(event.data)
+  }
+  mediaRecorder.onstop = (eve) => {
+    // Make blob out of our blobs, and open it.
+    let blob = new window.Blob(this.chunks, { 'type': 'audio/wav; codecs=1' })
+    blobToBase64(blob, (base64) => { // encod
+      let buf = new window.Buffer(base64, 'base64') // decod
+      console.log(buf)
+        /* fs.writeFile('./static/test.wav', buf, function (error) {
+            if (error) {
+                console.log(error)
+            } else {
+                return 'ok'
+            }
+        }) */
+    })
+  }
 }
 
 /**
-  * JavaScript cast functions
-  */
+ * JavaScript cast functions
+ */
 const toMMSSMS = () => {
-    var secnum = parseInt(this, 10) // don't forget the second param
-    var hours = Math.floor(secnum / 3600)
-    var minutes = Math.floor((secnum - (hours * 3600)) / 60)
-    var seconds = secnum - (hours * 3600) - (minutes * 60)
+  var secnum = parseInt(this, 10) // don't forget the second param
+  var hours = Math.floor(secnum / 3600)
+  var minutes = Math.floor((secnum - (hours * 3600)) / 60)
+  var seconds = secnum - (hours * 3600) - (minutes * 60)
 
-    // if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) { minutes = '0' + minutes }
-    if (seconds < 10) { seconds = '0' + seconds }
-    var time = /* hours+':'+ */ minutes + ':' + seconds
-    return time
+  // if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) { minutes = '0' + minutes }
+  if (seconds < 10) { seconds = '0' + seconds }
+  var time = /* hours+':'+ */ minutes + ':' + seconds
+  return time
 }
 
 export {
-    loginToParticle,
-    listDevices,
-    updateParticleDevices,
-    clearIntervalForTimer,
-    saveDeviceConfig,
-    saveSensorOfDeviceConfig,
-    analogRead,
-    digitalRead,
-    sendSignalUp,
-    sendSignalDown,
-    setDefaultLoopConfig,
-    createAndConnectOscillator,
-    createAndConnectSound,
-    blobToBase64,
-    recordLoop,
-    addLoop,
-    setMediaRecorderEventsForLoop,
-    loadSoundConfig,
-    toMMSSMS
+  loginToParticle,
+  listDevices,
+  updateParticleDevices,
+  clearIntervalForTimer,
+  saveDeviceConfig,
+  saveSensorOfDeviceConfig,
+  analogRead,
+  digitalRead,
+  sendSignalUp,
+  sendSignalDown,
+  setDefaultLoopConfig,
+  createAndConnectOscillator,
+  createAndConnectSound,
+  blobToBase64,
+  recordLoop,
+  addLoop,
+  setMediaRecorderEventsForLoop,
+  loadSoundConfig,
+  toMMSSMS
 }
--- app/vuex/store.js
+++ app/vuex/store.js
@@ -6,55 +6,55 @@ Vue.use(Vuex)
 let audio = new window.AudioContext()
 
 const state = {
-    particleToken: '',
-    particleDevices: [],
-    updateTimerIdForListDevices: null,
-    loops: [],
-    numberOfLoops: 0,
-    audioContext: audio,
-    loading: false,
-    error: '',
-    showError: false
+  particleToken: '',
+  particleDevices: [],
+  updateTimerIdForListDevices: null,
+  loops: [],
+  numberOfLoops: 0,
+  audioContext: audio,
+  loading: false,
+  error: '',
+  showError: false
 }
 
 const mutations = {
-    UPDATE_PARTICLE_TOKEN (state, token) {
-        state.particleToken = token
-    },
-    UPDATE_PARTICLE_DEVICE (state, deviceIndex, deviceInfo) {
-        Object.assign(state.particleDevices[deviceIndex], state.particleDevices[deviceIndex], deviceInfo)
-    },
-    PUSH_PARTICLE_DEVICE (state, deviceAttributes) {
-        state.particleDevices.push(deviceAttributes)
-    },
-    UPDATE_DEVICE_CONFIG (state, deviceIndex, sensorIndex, type, sensorValue) {
-        state.particleDevices[deviceIndex].sensors[sensorIndex][type] = sensorValue
-    },
-    TIMER_ID_OF_UPDATE_LIST_DEVICES (state, timerId) {
-        state.updateTimerIdForListDevices = timerId
-    },
-    CLEAR_INTERVAL (state, timerId) {
-        clearInterval(state.updateTimerIdForListDevices)
-    },
-    PUSH_NEW_LOOP (state, newLoop) {
-        state.numberOfLoops++
-        state.loops.push(newLoop)
-    },
-    UPDATE_BUFFER (state, buffer) {
-        state.buffer = buffer
-    },
-    SET_ERROR (state, error) {
-        state.error = error
-    },
-    SHOW_ERROR (state, boolean) {
-        state.showError = boolean
-    },
-    LOADING (state, boolean) {
-        state.loading = boolean
-    }
+  UPDATE_PARTICLE_TOKEN(state, token) {
+    state.particleToken = token
+  },
+  UPDATE_PARTICLE_DEVICE(state, deviceIndex, deviceInfo) {
+    Object.assign(state.particleDevices[deviceIndex], state.particleDevices[deviceIndex], deviceInfo)
+  },
+  PUSH_PARTICLE_DEVICE(state, deviceAttributes) {
+    state.particleDevices.push(deviceAttributes)
+  },
+  UPDATE_DEVICE_CONFIG(state, deviceIndex, sensorIndex, type, sensorValue) {
+    state.particleDevices[deviceIndex].sensors[sensorIndex][type] = sensorValue
+  },
+  TIMER_ID_OF_UPDATE_LIST_DEVICES(state, timerId) {
+    state.updateTimerIdForListDevices = timerId
+  },
+  CLEAR_INTERVAL(state, timerId) {
+    clearInterval(state.updateTimerIdForListDevices)
+  },
+  PUSH_NEW_LOOP(state, newLoop) {
+    state.numberOfLoops++
+      state.loops.push(newLoop)
+  },
+  UPDATE_BUFFER(state, buffer) {
+    state.buffer = buffer
+  },
+  SET_ERROR(state, error) {
+    state.error = error
+  },
+  SHOW_ERROR(state, boolean) {
+    state.showError = boolean
+  },
+  LOADING(state, boolean) {
+    state.loading = boolean
+  }
 }
 
 export default new Vuex.Store({
-    state,
-    mutations
+  state,
+  mutations
 })
